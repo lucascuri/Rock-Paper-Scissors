d087102d7865fe269c01981c890f5a25402fb455
Lucas Curi <lucascurigomes10@gmail.com>
2019-01-29 21:51:42 -0200

    Some Changes

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// first line on the app.js
+const userScore = 0;
+const compScore = 0;
+const userScoreDOC = document.getElementById('user-score');
+const compScoreDOC = document.getElementById('comp-score');
+
