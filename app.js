e3530bf2bb672a7ef6365d4b55eb4ded67f2d4dd
Lucas Curi <lucascurigomes10@gmail.com>
2019-01-30 22:06:47 -0200

    Some app implementations.

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const userScore = 0;
-const compScore = 0;
+const [userScore, compScore] = [0, 0];
 const userScoreDOC = document.getElementById('user-score');
 const compScoreDOC = document.getElementById('comp-score');
+const [numRock, numPaper, numScissor] = [1, 2, 3];
+
+const randomNumber = (min = 1, max = 3) => {
+    const num = Math.floor((Math.random() * 3) + 1);
+    console.log(num);
+}
+
+const roll = (rock = 1, paper = 2, scisssor = 3) => {
+    
+}
+
+$('.choice').on('click', function() {
+    randomNumber();
+});
 
