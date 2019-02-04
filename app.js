be27393ab3aa62ffed78d723d1cf49e5c3908b19
Lucas <lucascurigomes10@gmail.com>
2019-02-04 05:11:55 -0200

    upgrades on switch of function game()

--- app.js
+++ app.js
@@ -14,52 +14,105 @@ const getCompChoice = () => {
     return roll[random];
 }
 
-const result = () => {
-
+const result = (docResult, docScore, docScoreWinner, winner, m1, m2) => {
+    docResult.innerHTML = `${winner} wins! ${m1} covers ${m2}.`;
+    if (winner === 'User') {
+        docScore.innerHTML = `${docScoreWinner}:`;
+    } else {
+        docScore.innerHTML = `${docScoreWinner}`;
+    }
 }
 
+// const game = (userChoice) => {
+//     const compChoice = getCompChoice();
+//     switch (userChoice + compChoice) {
+//         case "rs":
+//         userScore++;
+//         result_p.innerHTML = `User Wins! Rock covers scissor.`;
+//         userScore_span.innerHTML = `${userScore}:`;
+//         break;
+
+//         case "pr":
+//         userScore++;
+//         result_p.innerHTML = `User Wins! Paper covers rock.`;
+//         userScore_span.innerHTML = `${userScore}:`;
+//         break;
+
+//         case "sp":
+//         userScore++;
+//         result_p.innerHTML = `User Wins! Scissor covers paper`;
+//         userScore_span.innerHTML = `${userScore}:`;
+//         break;
+
+//         case "rp":
+//         compScore++;
+//         result_p.innerHTML = `Computer Wins! Paper covers rock.`;
+//         compScore_span.innerHTML = `${compScore}`;
+//         break;
+
+//         case "ps":
+//         compScore++;
+//         result_p.innerHTML = `Computer Wins! Scissor covers paper.`;
+//         compScore_span.innerHTML = `${compScore}`;
+//         break;
+
+//         case "sr":
+//         compScore++;
+//         result_p.innerHTML = `Computer Wins! Rock covers Scissor.`;
+//         compScore_span.innerHTML = `${compScore}`;
+//         break;
+
+//         case "rr":
+//         case "pp":
+//         case "ss":
+//         userScore++; compScore++;
+//         result_p.innerHTML = `It's a draw!`;
+//         userScore_span.innerHTML = `${userScore}:`;
+//         compScore_span.innerHTML = `${compScore}`;
+//         break;
+//     }
+// }
+
 const game = (userChoice) => {
-    // console.log(`User: ${userChoice}, Computer: ${getCompChoice()}`);
     const compChoice = getCompChoice();
     switch (userChoice + compChoice) {
-        // user wins
         case "rs":
         userScore++;
-        result_p.innerHTML = `User Wins! Rock covers scissor.`;
-        userScore_span.innerHTML = `${userScore}:`;
+        result(result_p, userScore_span, userScore, 'User', 'Rock', 'Scissor');
         break;
+
         case "pr":
         userScore++;
-        result_p.innerHTML = `User Wins! Paper covers rock.`;
-        userScore_span.innerHTML = `${userScore}:`;
+        result(result_p, userScore_span, userScore, 'User', 'Paper', 'Rock');
         break;
+
         case "sp":
         userScore++;
-        result_p.innerHTML = `User Wins! Scissor covers paper`;
-        userScore_span.innerHTML = `${userScore}:`;
+        result(result_p, userScore_span, userScore, 'User', 'Scissor', 'Paper');
         break;
+
         case "rp":
         compScore++;
-        result_p.innerHTML = `Computer Wins! Paper covers rock.`;
-        compScore_span.innerHTML = `${compScore}`;
+        result(result_p, compScore_span, compScore, 'Comp', 'Paper', 'Rock');
         break;
+
         case "ps":
         compScore++;
-        result_p.innerHTML = `Computer Wins! Scissor covers paper.`;
-        compScore_span.innerHTML = `${compScore}`;
+        result(result_p, compScore_span, compScore, 'Comp', 'Scissor', 'Paper');
         break;
+
         case "sr":
         compScore++;
-        result_p.innerHTML = `Computer Wins! Rock covers Scissor.`;
-        compScore_span.innerHTML = `${compScore}`;
+        result(result_p, compScore_span, compScore, 'Comp', 'Rock', 'Scissor');
         break;
+
         case "rr":
         case "pp":
         case "ss":
         userScore++; compScore++;
         result_p.innerHTML = `It's a draw!`;
         userScore_span.innerHTML = `${userScore}:`;
-        compScore_span.innerHTML = `${compScore}`;
+        compScore_span.innerHTML = `${compScore}`; 
         break;
     }
 }
